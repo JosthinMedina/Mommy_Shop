0071ef0aab03d2ab4722681d3f3b1c1b340b38b7
justh <justh@LAPTOP-LNLIR2AK>
2023-10-01 09:59:21 -0600

    Trabajo de mi tienda

---
+++ src/main/java/com/Mom_Tienda/MomTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.Mom_Tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomTiendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mom_Tienda/MomTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mom_Tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
